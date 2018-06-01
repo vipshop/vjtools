2ae9e505759ec00ac2641ed0c48b862327ee7acd
DiamondYuan <541832074@qq.com>
2018-06-01 17:40:38 +0800

    删除不需要的 '\n'
    不加也会换行。加了以后 PDF 里面会有这个符号

--- standard/docs/merge.sh
+++ standard/docs/merge.sh
@@ -1,32 +1,18 @@
 #!/bin/sh
 
 cat ../README.md > vip-java-standard.md
-echo '\n' >> vip-java-standard.md
 cat chapter01.md >> vip-java-standard.md
-echo '\n' >> vip-java-standard.md
 cat chapter02.md >> vip-java-standard.md
-echo '\n' >> vip-java-standard.md
 cat chapter03.md >> vip-java-standard.md
-echo '\n' >> vip-java-standard.md
 cat chapter04.md >> vip-java-standard.md
-echo '\n' >> vip-java-standard.md
 cat chapter05.md >> vip-java-standard.md
-echo '\n' >> vip-java-standard.md
 cat chapter06.md >> vip-java-standard.md
-echo '\n' >> vip-java-standard.md
 cat chapter07.md >> vip-java-standard.md
-echo '\n' >> vip-java-standard.md
 cat chapter08.md >> vip-java-standard.md
-echo '\n' >> vip-java-standard.md
 cat chapter09.md >> vip-java-standard.md
-echo '\n' >> vip-java-standard.md
 cat chapter10.md >> vip-java-standard.md
-echo '\n' >> vip-java-standard.md
 cat chapter11.md >> vip-java-standard.md
-echo '\n' >> vip-java-standard.md
 cat chapter12.md >> vip-java-standard.md
-echo '\n' >> vip-java-standard.md
 cat ali.md >> vip-java-standard.md
-
 
 echo 'upload vip-java-standard.md to http://www.mdtr2pdf.com'
