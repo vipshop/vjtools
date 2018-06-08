be33753b976bf4da919edf79a734d689ed1515ed
calvin.xiao <calvin.xiao@vipshop.com>
2018-06-08 17:54:56 +0800

    #28 fix testcase

--- vjkit/src/test/java/com/vip/vjtools/vjkit/base/PropertiesUtilTest.java
+++ vjkit/src/test/java/com/vip/vjtools/vjkit/base/PropertiesUtilTest.java
@@ -10,7 +10,7 @@ public class PropertiesUtilTest {
 
 	@Test
 	public void loadProperties() {
-		Properties p1 = PropertiesUtil.loadFromFile("classpath://application.properties");
+		Properties p1 = PropertiesUtil.loadFromFile("classpath:application.properties");
 		assertThat(p1.get("springside.min")).isEqualTo("1");
 		assertThat(p1.get("springside.max")).isEqualTo("10");
 
@@ -32,8 +32,6 @@ public class PropertiesUtilTest {
 		assertThat(PropertiesUtil.getString(p2, "springside.maxA", "")).isEqualTo("");
 
 		assertThat(PropertiesUtil.getBoolean(p2, "isOpen", false)).isTrue();
-		
-		
 	}
 
 }
