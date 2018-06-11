355c13a37be30c63a8bcb8757d6fcb2f99260457
Robin Gong <i.robingong@gmail.com>
2018-06-11 09:12:12 +0800

    [vjkit] specify scope for @NotNull and @Nullable

--- vjkit/src/main/java/com/vip/vjtools/vjkit/base/annotation/NotNull.java
+++ vjkit/src/main/java/com/vip/vjtools/vjkit/base/annotation/NotNull.java
@@ -1,8 +1,12 @@
 package com.vip.vjtools.vjkit.base.annotation;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Target;
+
 /**
  * 标注参数不可为Null
  */
+@Target(ElementType.PARAMETER)
 public @interface NotNull {
 
 }
