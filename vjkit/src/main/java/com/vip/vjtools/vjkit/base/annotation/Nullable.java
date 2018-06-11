a3020dbd363513ad39f5afdba28fe2fd6299fcaa
Robin Gong <i.robingong@gmail.com>
2018-06-11 12:15:41 +0800

    [vjkit] expand scope for @NotNull and @Nullable according to usage

--- vjkit/src/main/java/com/vip/vjtools/vjkit/base/annotation/Nullable.java
+++ vjkit/src/main/java/com/vip/vjtools/vjkit/base/annotation/Nullable.java
@@ -4,9 +4,9 @@ import java.lang.annotation.ElementType;
 import java.lang.annotation.Target;
 
 /**
- * 标注参数可为Null
+ * 标注参数、属性、方法可为 Null
  */
-@Target(ElementType.PARAMETER)
+@Target({ElementType.PARAMETER, ElementType.FIELD, ElementType.METHOD})
 public @interface Nullable {
 
 }
