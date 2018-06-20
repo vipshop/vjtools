8a665b87545f30adf8a7ce633547ba95c773321c
calvin.xiao <calvin.xiao@vipshop.com>
2018-06-20 18:23:58 +0800

    backj to 1.0.1, sonatype 又check不过。

--- vjmap/src/main/java/com/vip/vjtools/vjmap/VJMap.java
+++ vjmap/src/main/java/com/vip/vjtools/vjmap/VJMap.java
@@ -13,7 +13,7 @@ import sun.jvm.hotspot.runtime.VM;
 
 public class VJMap {
 
-	public static final String VERSION = "1.0.2";
+	public static final String VERSION = "1.0.1";
 
 	public static void runHeapVisitor(int pid, boolean orderByName, long minSize) {
 		ObjectHeap heap = VM.getVM().getObjectHeap();
@@ -104,7 +104,7 @@ public class VJMap {
 			} else if (modeFlag.startsWith("-address")) {
 				printGenAddress();
 			} else if (modeFlag.startsWith("-version")) {
-				System.out.println("vjmap version:"＋VERSION);
+				System.out.println("vjmap version:" + VERSION);
 				return;
 			} else {
 				printHelp();
--- vjtop/src/main/java/com/vip/vjtools/vjtop/VJTop.java
+++ vjtop/src/main/java/com/vip/vjtools/vjtop/VJTop.java
@@ -20,7 +20,7 @@ import joptsimple.OptionSet;
  */
 public class VJTop {
 
-	public static final String VERSION = "1.0.2";
+	public static final String VERSION = "1.0.1";
 
 	public static final int DEFAULT_INTERVAL = 10;
 
