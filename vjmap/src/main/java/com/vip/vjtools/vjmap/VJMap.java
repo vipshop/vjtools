40b1640c3d8a615be52a7d2af8c71686306151f4
calvin.xiao <calvin.xiao@vipshop.com>
2018-06-25 18:56:53 +0800

    #49 移植TBJMap的printClassURL功能并支持JDK8

--- vjmap/src/main/java/com/vip/vjtools/vjmap/VJMap.java
+++ vjmap/src/main/java/com/vip/vjtools/vjmap/VJMap.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import com.vip.vjtools.vjmap.oops.GenAddressAccessor;
 import com.vip.vjtools.vjmap.oops.HeapHistogramVisitor;
+import com.vip.vjtools.vjmap.oops.LoadedClassAccessor;
 import com.vip.vjtools.vjmap.oops.OldgenAccessor;
 import com.vip.vjtools.vjmap.oops.SurvivorAccessor;
 
@@ -13,7 +14,7 @@ import sun.jvm.hotspot.runtime.VM;
 
 public class VJMap {
 
-	public static final String VERSION = "1.0.1";
+	public static final String VERSION = "1.0.2";
 
 	public static void runHeapVisitor(int pid, boolean orderByName, long minSize) {
 		ObjectHeap heap = VM.getVM().getObjectHeap();
@@ -50,6 +51,11 @@ public class VJMap {
 	public static void printGenAddress() {
 		GenAddressAccessor accessor = new GenAddressAccessor();
 		accessor.printHeapAddress();
+	}
+
+	public static void printLoadedClass() {
+		LoadedClassAccessor accessor = new LoadedClassAccessor();
+		accessor.pringLoadedClass();
 	}
 
 	public static void main(String[] args) {
@@ -103,6 +109,8 @@ public class VJMap {
 				runOldGenAccessor(orderByName, minSize);
 			} else if (modeFlag.startsWith("-address")) {
 				printGenAddress();
+			} else if (modeFlag.startsWith("-class")) {
+				printLoadedClass();
 			} else if (modeFlag.startsWith("-version")) {
 				System.out.println("vjmap version:" + VERSION);
 				return;
@@ -146,5 +154,6 @@ public class VJMap {
 		System.out.printf(format, "-sur:minsize=1024,byname",
 				"print survivor histogram, age>=3, survivor size>=1024, order by class name");
 		System.out.printf(format, "-address", "print address for all gens");
+		System.out.printf(format, "-class", "print all loaded classes");
 	}
 }
---
+++ vjmap/src/main/java/com/vip/vjtools/vjmap/oops/LoadedClassAccessor.java
@@ -0,0 +1,58 @@
+package com.vip.vjtools.vjmap.oops;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+
+import sun.jvm.hotspot.debugger.AddressException;
+import sun.jvm.hotspot.memory.SystemDictionary;
+import sun.jvm.hotspot.oops.InstanceKlass;
+import sun.jvm.hotspot.oops.Klass;
+import sun.jvm.hotspot.oops.Oop;
+import sun.jvm.hotspot.oops.OopField;
+import sun.jvm.hotspot.oops.OopUtilities;
+import sun.jvm.hotspot.runtime.VM;
+
+public class LoadedClassAccessor {
+
+	public void pringLoadedClass() {
+		try {
+			System.err.println("Finding classes in System Dictionary..");
+			final ArrayList<InstanceKlass> klasses = new ArrayList<InstanceKlass>(128);
+
+			SystemDictionary dict = VM.getVM().getSystemDictionary();
+			dict.classesDo(new SystemDictionary.ClassVisitor() {
+				public void visit(Klass k) {
+					if (k instanceof InstanceKlass) {
+						klasses.add((InstanceKlass) k);
+					}
+				}
+			});
+
+			Collections.sort(klasses, new Comparator<InstanceKlass>() {
+				public int compare(InstanceKlass x, InstanceKlass y) {
+					return x.getName().asString().compareTo(y.getName().asString());
+				}
+			});
+
+			System.out.println("#class             #loader");
+			System.out.println("-----------------------------------------------");
+			for (InstanceKlass k : klasses) {
+				System.out.printf("%s, %s\n", getClassNameFrom(k), getClassLoaderOopFrom(k));
+			}
+		} catch (AddressException e) {
+			System.err.println("Error accessing address 0x" + Long.toHexString(e.getAddress()));
+			e.printStackTrace();
+		}
+	}
+
+	private static String getClassLoaderOopFrom(InstanceKlass klass) {
+		Oop loader = klass.getClassLoader();
+		return loader != null ? getClassNameFrom((InstanceKlass) loader.getKlass()) + " @ " + loader.getHandle()
+				: "<bootstrap>";
+	}
+
+	private static String getClassNameFrom(InstanceKlass klass) {
+		return klass != null ? klass.getName().asString().replace('/', '.') : null;
+	}
+}
