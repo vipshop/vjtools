3350813451e2f0f40fad6d1746b9560a2c1cf353
calvin.xiao <calvin.xiao@vipshop.com>
2018-07-19 18:04:13 +0800

    #44 建议的JDK参数设定, 更新注释

--- vjstar/src/main/script/jvm-options/jvm-options.sh
+++ vjstar/src/main/script/jvm-options/jvm-options.sh
@@ -1,7 +1,7 @@
 
 #!/bin/bash
 
-## Only for JDK7/JDK8
+
 
 # change the dir here
 LOGDIR="./logs"
@@ -31,11 +31,11 @@ MEM_OPTS="$MEM_OPTS -XX:+AlwaysPreTouch"
 # 堆外内存的最大值默认约等于堆大小，可以显式将其设小，获得一个比较清晰的内存总量预估
 #MEM_OPTS="$MEM_OPTS -XX:MaxDirectMemorySize=2g"
 
-# 根据VJTop的观察，调整二进制代码区大小避免满了之后不能再JIT
+# 根据JMX/VJTop的观察，调整二进制代码区大小避免满了之后不能再JIT，JDK7/8，是否打开多层编译的默认值都不一样
 #MEM_OPTS="$MEM_OPTS -XX:ReservedCodeCacheSize=240M"
 
 
-## GC Options ##
+## GC Options##
 
 GC_OPTS="-XX:+UseConcMarkSweepGC -XX:CMSInitiatingOccupancyFraction=75 -XX:+UseCMSInitiatingOccupancyOnly"
 
@@ -45,16 +45,17 @@ GC_OPTS="$GC_OPTS -XX:+ExplicitGCInvokesConcurrent"
 # CMS中的下列阶段并发执行
 GC_OPTS="$GC_OPTS -XX:+ParallelRefProcEnabled -XX:+CMSParallelInitialMarkEnabled"
 
-# 根据应用的对象生命周期设定，减少实际老生代对象在新生代停留时间，加快YGC速度
+# 根据应用的对象生命周期设定，减少事实上的老生代对象在新生代停留时间，加快YGC速度
 GC_OPTS="$GC_OPTS -XX:MaxTenuringThreshold=3"
 
 # 如果OldGen较大，加大YGC时扫描OldGen关联的卡片，加快YGC速度，默认值256较低
 GC_OPTS="$GC_OPTS -XX:+UnlockDiagnosticVMOptions -XX:ParGCCardsPerStrideChunk=1024"
 
-# 如果机器上有其他较繁忙的进程在运行时，将GC线程数设置得比默认值(CPU核数＊5/8 )更低。另建议CMS GC线程数简单改为YGC线程数一半.
+# 如果JVM并不独占机器，机器上有其他较繁忙的进程在运行，将GC线程数设置得比默认值(CPU核数＊5/8 )更低以减少竞争，反而会大大加快YGC速度。
+# 另建议CMS GC线程数简单改为YGC线程数一半.
 #GC_OPTS="$GC_OPTS -XX:ParallelGCThreads=12 -XX:ConcGCThreads=6"
 
-# 如果CMS GC时间很长，并且明显受新生代存活对象数量影响时打开
+# 如果CMS GC时间很长，并且明显受新生代存活对象数量影响时打开，但会导致每次CMS GC与一次YGC连在一起执行，加大了事实上JVM停顿的时间。
 #GC_OPTS="$GC_OPTS -XX:+CMSScavengeBeforeRemark"
 
 # 如果永久代不会增长，关闭ClassUnloading降低CMS GC时出现缓慢的几率
@@ -63,9 +64,9 @@ GC_OPTS="$GC_OPTS -XX:+UnlockDiagnosticVMOptions -XX:ParGCCardsPerStrideChunk=10
 #fi
 
 
-## GC log Options ##
+## GC log Options, only for JDK7/JDK8##
 
-# change the name here，默认使用/dev/shm 内存文件系统避免在高IO场景下写GC LOG时被阻塞
+# change the file name here，默认使用/dev/shm 内存文件系统避免在高IO场景下写GC日志时被阻塞导致STW时间延长
 if [ -d /dev/shm/ ]; then
     GC_LOG_FILE=/dev/shm/gc-myapp.log
 else
@@ -74,7 +75,7 @@ fi
 
 
 if [ -f ${GC_LOG_FILE} ]; then
-  # change the name here
+  # change the file name here
   GC_LOG_BACKUP =  ${LOGDIR}/gc-myapp-$(date +'%Y%m%d_%H%M%S').log
   echo "saving gc log ${GC_LOG_FILE} to ${GC_LOG_BACKUP}"
   mv ${GC_LOG_FILE} ${GC_LOG_BACKUP}
@@ -89,7 +90,7 @@ if [[ "$JAVA_VERSION" < "1.8" ]]; then
 	GCLOG_OPTS="$GCLOG_OPTS -XX:+PrintGCCause"
 fi
 
-# 打印安全点日志
+# 打印安全点日志，找出GC日志里非GC的停顿的原因
 #GCLOG_OPTS="$GCLOG_OPTS -XX:+PrintSafepointStatistics -XX:PrintSafepointStatisticsCount=1"
 
 
@@ -97,10 +98,8 @@ fi
 
 OPTIMIZE_OPTS="-XX:-UseBiasedLocking -XX:AutoBoxCacheMax=20000 -Djava.security.egd=file:/dev/./urandom"
 
-# 如果希望最终JIT所有函数，无论函数的热度如何。
-#OPTIMIZE_OPTS="$OPTIMIZE_OPTS -XX:-UseCounterDecay"
 
-# 关闭PerfData写入，避免高IO场景GC时因为写PerfData文件被阻塞，但会导致jstats，jps不能使用。
+# 关闭PerfData写入，避免高IO场景GC时因为写PerfData文件被阻塞，但会使得jstats，jps不能使用。
 #OPTIMIZE_OPTS="$OPTIMIZE_OPTS -XX:+PerfDisableSharedMem"
 
 # 关闭多层编译，减少应用刚启动时的JIT导致的可能超时，以及避免部分函数C1编译后最终没被C2编译。 但导致函数没有被初始C1编译。
@@ -108,6 +107,8 @@ OPTIMIZE_OPTS="-XX:-UseBiasedLocking -XX:AutoBoxCacheMax=20000 -Djava.security.e
 #  OPTIMIZE_OPTS="$OPTIMIZE_OPTS -XX:-TieredCompilation"
 #fi
 
+# 如果希望无论函数的热度如何，最终JIT所有函数，关闭GC时将函数调用次数减半。
+#OPTIMIZE_OPTS="$OPTIMIZE_OPTS -XX:-UseCounterDecay"
 
 ## Trouble shotting Options##
 
@@ -120,7 +121,7 @@ SHOTTING_OPTS="-XX:+PrintCommandLineFlags -XX:-OmitStackTraceInFastThrow -XX:Err
 
 ## JMX Options##
 
-#开放JMX本地访问
+#开放JMX本地访问，设定端口号
 JMX_OPTS="-Djava.rmi.server.hostname=127.0.0.1 -Dcom.sun.management.jmxremote.port=7001 -Dcom.sun.management.jmxremote -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false"
 
 
@@ -131,6 +132,6 @@ OTHER_OPTS="-Djava.net.preferIPv4Stack=true -Djava.awt.headless=true"
 
 ## All together ##
 
-JAVA_OPTS="$MEM_OPTS $GC_OPTS $GCLOG_OPTS $OPTIMIZE_OPTS $SHOTTING_OPTS $JMX_OPTS $OTHER_OPTS"
+export JAVA_OPTS="$MEM_OPTS $GC_OPTS $GCLOG_OPTS $OPTIMIZE_OPTS $SHOTTING_OPTS $JMX_OPTS $OTHER_OPTS"
 
 echo JAVA_OPTS=$JAVA_OPTS
