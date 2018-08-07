7cd0c55ed7b0c018bd93bb19c4b202811e9c361f
calvin.xiao <calvin.xiao@vipshop.com>
2018-08-07 19:17:01 +0800

    upto 1.0.3

--- vjmap/src/main/java/com/vip/vjtools/vjmap/VJMap.java
+++ vjmap/src/main/java/com/vip/vjtools/vjmap/VJMap.java
@@ -15,7 +15,7 @@ import sun.jvm.hotspot.runtime.VM;
 
 public class VJMap {
 
-	public static final String VERSION = "1.0.2";
+	public static final String VERSION = "1.0.3";
 
 	private static PrintStream tty = System.out;
 
