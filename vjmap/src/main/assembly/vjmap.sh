afa55bb947cfd5540a0b977cd37f14fb65db2e89
calvin.xiao <calvin.xiao@vipshop.com>
2018-08-07 18:08:25 +0800

    #63 继续统一Tools中的脚本，小幅优化启动参数

--- vjmap/src/main/assembly/vjmap.sh
+++ vjmap/src/main/assembly/vjmap.sh
@@ -16,7 +16,7 @@ fi
 echo -e "\033[31mWARNING!! STW(Stop-The-World) will be performed on your Java process, if this is NOT wanted, type 'Ctrl+C' to exit. \033[0m"
 
 DIR=$( cd $(dirname $0) ; pwd -P )
-JAVA_OPTS="-Xms512m -Xmx512m -Xmn400m -Xss256k -XX:+UseSerialGC -XX:+TieredCompilation -XX:CICompilerCount=2 -Xverify:none -XX:AutoBoxCacheMax=20000"
+JAVA_OPTS="-Xms512m -Xmx512m -Xmn400m -XX:+UseConcMarkSweepGC -XX:+TieredCompilation -Xverify:none -XX:AutoBoxCacheMax=20000"
 
 
 "$JAVA_HOME"/bin/java $JAVA_OPTS -classpath $DIR/vjmap.jar:$SAJDI_PATH com.vip.vjtools.vjmap.VJMap $*
