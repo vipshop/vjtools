5c27d752d6ccfee6841d80383b729b2007f15a3a
calvin.xiao <calvin.xiao@vipshop.com>
2018-12-18 17:25:22 +0800

    准备发布1.0.8

--- vjtop/src/main/java/com/vip/vjtools/vjtop/VJTop.java
+++ vjtop/src/main/java/com/vip/vjtools/vjtop/VJTop.java
@@ -19,7 +19,7 @@ import joptsimple.OptionSet;
 
 public class VJTop {
 
-	public static final String VERSION = "1.0.7";
+	public static final String VERSION = "1.0.8";
 
 	public VMDetailView view;
 	private Thread mainThread;
