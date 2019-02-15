cf081e896436e20d808e8705dce141f5388b101b
baifenghe <1170126481@qq.com>
2019-02-15 10:39:41 +0800

    add merge.bat

---
+++ docs/standard/merge.bat
@@ -0,0 +1,22 @@
+@echo off
+type README.md > vip-java-standard.md
+type chapter01.md >> vip-java-standard.md
+type chapter02.md >> vip-java-standard.md
+type chapter03.md >> vip-java-standard.md
+type chapter04.md >> vip-java-standard.md
+type chapter05.md >> vip-java-standard.md
+type chapter06.md >> vip-java-standard.md
+type chapter07.md >> vip-java-standard.md
+type chapter08.md >> vip-java-standard.md
+type chapter09.md >> vip-java-standard.md
+type chapter10.md >> vip-java-standard.md
+type chapter11.md >> vip-java-standard.md
+type chapter12.md >> vip-java-standard.md
+type ali.md >> vip-java-standard.md
+
+echo merge success!!!
+echo upload vip-java-standard.md to http://www.mdtr2pdf.com
+echo press any key to exit.
+
+pause > nul
+exit
